979629fca99c71f60ff2deb6d2823086b50c26bb
Softlabltd.com <freelancernishad123@gmail.com>
2023-04-08 12:50:36 +0600

    update

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SUN ENERGY</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('sun.jpg') }}">
+    <title>united-eib3s</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('united.jpg') }}">
     <!-- Bootstrap CSS -->
     <link href="{{ asset('frontend/css/bootstrap.min.css') }}" rel="stylesheet" >
     <link href="{{ asset('frontend/css/font-awesome.min.css') }}" rel="stylesheet" >
